ae3c43066828816f338a33dd7b2e78a7762a50ba
Sven Hübner <sven.huebner.it@gmx.de>
2019-07-05 19:34:25 +0200

    add expression-based implementation

--- ValueObject.Test/ValueObjectTests.cs
+++ ValueObject.Test/ValueObjectTests.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -19,7 +20,7 @@ namespace ValueObject.Test
         [Fact]
         public void ObjectEquals_When_other_is_null_Then_returns_false()
         {
-            var isEqual = ((object)new EmptyValueObject()).Equals(null);
+            var isEqual = ((object)new ValueObjectWithAPrimitive("42")).Equals(null);
 
             isEqual.Should().BeFalse();
         }
@@ -27,7 +28,7 @@ namespace ValueObject.Test
         [Fact]
         public void ObjectEquals_When_other_is_of_other_type_Then_returns_false()
         {
-            var isEqual = ((object)new EmptyValueObject()).Equals("42");
+            var isEqual = ((object)new ValueObjectWithAPrimitive("42")).Equals("42");
 
             isEqual.Should().BeFalse();
         }
@@ -35,8 +36,16 @@ namespace ValueObject.Test
         [Fact]
         public void ObjectEquals_When_other_is_same_reference_Then_returns_true()
         {
-            object valueObject = new EmptyValueObject();
+            object valueObject = new ValueObjectWithAPrimitive("42");
             var isEqual = ((object)valueObject).Equals(valueObject);
+
+            isEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_When_valueobject_has_no_properties_and_other_is_different_reference_Then_returns_true()
+        {
+            var isEqual = new EmptyValueObject().Equals(new EmptyValueObject());
 
             isEqual.Should().BeTrue();
         }
@@ -44,7 +53,7 @@ namespace ValueObject.Test
         [Fact]
         public void Equals_When_other_is_null_Then_returns_false()
         {
-            var isEqual = new EmptyValueObject().Equals(null);
+            var isEqual = new ValueObjectWithAPrimitive("42").Equals(null);
 
             isEqual.Should().BeFalse();
         }
@@ -52,7 +61,7 @@ namespace ValueObject.Test
         [Fact]
         public void Equals_When_other_is_same_reference_Then_returns_true()
         {
-            object valueObject = new EmptyValueObject();
+            object valueObject = new ValueObjectWithAPrimitive("42");
             var isEqual = valueObject.Equals(valueObject);
 
             isEqual.Should().BeTrue();
@@ -61,8 +70,8 @@ namespace ValueObject.Test
         [Fact]
         public void Equals_When_primitive_value_is_different_Then_returns_false()
         {
-            var isEqual = new ValueObjectWithPrimitives("foo")
-                .Equals(new ValueObjectWithPrimitives("bar"));
+            var isEqual = new ValueObjectWithAPrimitive("foo")
+                .Equals(new ValueObjectWithAPrimitive("bar"));
 
             isEqual.Should().BeFalse();
         }
@@ -70,8 +79,8 @@ namespace ValueObject.Test
         [Fact]
         public void Equals_When_primitive_value_is_equal_Then_returns_true()
         {
-            var isEqual = new ValueObjectWithPrimitives("foo")
-                .Equals(new ValueObjectWithPrimitives("foo"));
+            var isEqual = new ValueObjectWithAPrimitive("foo")
+                .Equals(new ValueObjectWithAPrimitive("foo"));
 
             isEqual.Should().BeTrue();
         }
@@ -79,57 +88,113 @@ namespace ValueObject.Test
         [Fact]
         public void Equals_When_primitive_values_are_different_Then_returns_false()
         {
-            var isEqual = new ValueObjectWithPrimitives("foo").Equals(new ValueObjectWithPrimitives("bar"));
+            var isEqual = new ValueObjectWithAPrimitive("foo")
+                .Equals(new ValueObjectWithAPrimitive("bar"));
 
             isEqual.Should().BeFalse();
         }
 
         [Fact]
+        [Trait("Category", "Performance")]
         public void Equals_is_an_order_of_magnitude_faster_reflection()
         {
-            var foo = new ValueObjectWithPrimitives("foo");
-            var bar = new ValueObjectWithPrimitives("bar");
+            var foo = new ValueObjectWithManyPrimitives(
+                "foo",
+                "foo",
+                "foo",
+                "foo",
+                "foo",
+                "foo",
+                "foo");
+            var bar = new ValueObjectWithManyPrimitives(
+                "foo",
+                "foo",
+                "foo",
+                "foo",
+                "foo",
+                "foo",
+                "bar");
+
+            // init
+            ReflectionEqualsHelper<ValueObjectWithManyPrimitives>
+                .ReflectionEquals(foo, bar);
+            const long interations = 1_000_000;
 
             var stopwatch = new Stopwatch();
-            stopwatch.Start();
 
-            for (var i = 0; i < 1_000_000; i++)
+            stopwatch.Start();
+            for (var i = 0; i < interations; i++)
             {
-                ReflectionEquals(foo, bar);
+                ReflectionEqualsHelper<ValueObjectWithManyPrimitives>
+                    .ReflectionEquals(foo, bar);
             }
-            var reflectionEqualsMilliSeconds = stopwatch.ElapsedMilliseconds;
-            stopwatch.Start();
 
-            for (var i = 0; i < 1_000_000; i++)
+            var reflectionEqualsMilliSeconds = stopwatch.ElapsedMilliseconds;
+            output.WriteLine($"reflection: {reflectionEqualsMilliSeconds} ms");
+
+            stopwatch.Restart();
+            for (var i = 0; i < interations; i++)
             {
                 foo.Equals(bar);
             }
 
             var equalsMilliseconds = stopwatch.ElapsedMilliseconds;
+            output.WriteLine($"equals: {equalsMilliseconds} ms");
+
             equalsMilliseconds.Should().BeLessThan(
                 reflectionEqualsMilliSeconds / 10);
         }
+    }
 
-        private static bool ReflectionEquals<T>(T one, T other)
-        {
-            var properties = typeof(T).GetProperties();
+    public static class ReflectionEqualsHelper<T>
+    {
+        private static readonly PropertyInfo[] properties = typeof(T).GetProperties();
 
-            return properties.All(prop =>
+        public static bool ReflectionEquals(T one, T other) =>
+            properties.All(prop =>
                 Equals(prop.GetValue(one), prop.GetValue(other)));
-        }
     }
 
     public class EmptyValueObject : ValueObject<EmptyValueObject>
     {
     }
 
-    public class ValueObjectWithPrimitives : ValueObject<ValueObjectWithPrimitives>
+    public class ValueObjectWithAPrimitive : ValueObject<ValueObjectWithAPrimitive>
     {
-        public ValueObjectWithPrimitives(string value)
+        public ValueObjectWithAPrimitive(string value1)
         {
-            Value = value ?? throw new ArgumentNullException(value);
+            Value1 = value1 ?? throw new ArgumentNullException(nameof(value1));
         }
 
-        public string Value { get; }
+        public string Value1 { get; }
+    }
+
+    public class ValueObjectWithManyPrimitives : ValueObject<ValueObjectWithManyPrimitives>
+    {
+        public ValueObjectWithManyPrimitives(
+            string value1,
+            string value2,
+            string value3,
+            string value4,
+            string value5,
+            string value6,
+            string value7)
+        {
+            Value1 = value1 ?? throw new ArgumentNullException(nameof(value1));
+            Value2 = value2 ?? throw new ArgumentNullException(nameof(value2));
+            Value3 = value3 ?? throw new ArgumentNullException(nameof(value3));
+            Value4 = value4 ?? throw new ArgumentNullException(nameof(value4));
+            Value5 = value5 ?? throw new ArgumentNullException(nameof(value5));
+            Value6 = value6 ?? throw new ArgumentNullException(nameof(value6));
+            Value7 = value7 ?? throw new ArgumentNullException(nameof(value7));
+        }
+
+        public string Value1 { get; }
+        public string Value2 { get; }
+        public string Value3 { get; }
+        public string Value4 { get; }
+        public string Value5 { get; }
+        public string Value6 { get; }
+        public string Value7 { get; }
     }
 }
--- ValueObject/ValueObject.cs
+++ ValueObject/ValueObject.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace ValueObject
 {
     public abstract class ValueObject<T> : IEquatable<T>
         where T : ValueObject<T>
     {
-        private static readonly Func<T, T, bool> GenericEquals = GenerateGenericEquals();
+        private static readonly ParameterExpression OneParam = Expression.Parameter(typeof(T), "one");
+        private static readonly ParameterExpression OtherParam = Expression.Parameter(typeof(T), "other");
 
         private static readonly string[] PropertyNames = typeof(T).GetProperties()
                 .Select(p => p.Name)
                 .ToArray();
+
+        private static readonly Func<T, T, bool> GenericEquals = PropertyNames.Any()
+            ? GenerateGenericEquals()
+            : (one, other) => true;
 
         public override bool Equals(object obj) =>
             ReferenceEquals(obj, this) ||
@@ -18,19 +24,31 @@ namespace ValueObject
             Equals(other));
 
         public bool Equals(T other) =>
-            ReferenceEquals(other, this) ||
-            (other is ValueObject<T> otherValueObject &&
-            GenericEquals(this, otherValueObject));
+            other != null &&
+            (ReferenceEquals(other, this) ||
+            GenericEquals(this, other));
 
         public static implicit operator T(ValueObject<T> valueObject) =>
             (T)valueObject;
 
         private static Func<T, T, bool> GenerateGenericEquals()
         {
-            var properties = typeof(T).GetProperties();
+            Expression andExpression = PropertyNames.Skip(1).Aggregate
+                (EqualsExpr(PropertyNames.First()),
+                (exp, propName) => Expression.AndAlso(
+                        exp,
+                        EqualsExpr(propName)));
 
-            return (one, other) => properties.All(prop =>
-                Equals(prop.GetValue(one), prop.GetValue(other)));
+            var equalsExpression = Expression.Lambda<Func<T, T, bool>>(
+                andExpression,
+                new[] { OneParam, OtherParam });
+
+            return equalsExpression.Compile();
+
+            BinaryExpression EqualsExpr(string propertyName) =>
+                Expression.Equal(
+                    Expression.Property(OneParam, propertyName),
+                    Expression.Property(OtherParam, propertyName));
         }
     }
 }
