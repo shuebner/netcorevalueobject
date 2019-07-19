9875cc59c3931209d2680d4bf602b66bf4259cb4
Sven Hübner <sven.huebner.it@gmx.de>
2019-07-19 13:12:45 +0200

    simplify DeepValueEquals

--- ValueObject/DeepValueEquals.cs
+++ ValueObject/DeepValueEquals.cs
@@ -9,181 +9,109 @@ namespace ValueObject
 {
     public static class DeepValueEquals
     {
+        private delegate MemberExpression GetMemberAccessor(Expression param, string name);
+        
         private static readonly Type[] ValidSequenceTypes = { typeof(ImmutableArray<>), typeof(ImmutableList<>) };
         private static readonly Type[] ValueTypeEquivalents = { typeof(string) };
 
-        public static Func<T, T, bool> FromFields<T>()
-        {
-            ParameterExpression OneParam = Expression.Parameter(typeof(T), "one");
-            ParameterExpression OtherParam = Expression.Parameter(typeof(T), "other");
+        public static Func<T, T, bool> FromFields<T>() =>
+            ForMembers<T, FieldInfo>(typeof(T).GetFields(), fieldInfo => fieldInfo.FieldType, Expression.Field);
 
-            FieldInfo[] fieldInfos = typeof(T).GetFields();
-            if (!fieldInfos.Any())
+        public static Func<T, T, bool> FromProperties<T>() =>
+            ForMembers<T, PropertyInfo>(typeof(T).GetProperties(), propertyInfo => propertyInfo.PropertyType, Expression.Property);
+
+        private static Func<T, T, bool> ForMembers<T, TMember>(
+            IEnumerable<TMember> memberInfos,
+            Func<TMember, Type> getMemberType,
+            GetMemberAccessor getMemberAccessor)
+            where TMember : MemberInfo
+        {
+            ParameterExpression oneParam = Expression.Parameter(typeof(T), "one");
+            ParameterExpression otherParam = Expression.Parameter(typeof(T), "other");
+
+            if (!memberInfos.Any())
             {
                 return Expression.Lambda<Func<T, T, bool>>(
                     Expression.Constant(true),
-                    OneParam,
-                    OtherParam)
+                    oneParam,
+                    otherParam)
                     .Compile();
             }
 
-            var equalityExpressions = fieldInfos.Select(EqualityExpr);
+            var equalityExpressions = memberInfos.Select(memberInfo =>
+                EqualityExpr(oneParam, otherParam, memberInfo.Name, getMemberType(memberInfo), getMemberAccessor));
             var andExpression = equalityExpressions.Aggregate(Expression.AndAlso);
 
             Expression<Func<T, T, bool>> equalsExpression = Expression.Lambda<Func<T, T, bool>>(
                 andExpression,
-                new[] { OneParam, OtherParam });
+                new[] { oneParam, otherParam });
 
             return equalsExpression.Compile();
-
-            Expression EqualityExpr(FieldInfo fieldInfo)
-            {
-                var fieldName = fieldInfo.Name;
-                var fieldType = fieldInfo.FieldType;
-
-                if (fieldType.IsGenericType && ValidSequenceTypes.Contains(fieldType.GetGenericTypeDefinition()))
-                {
-                    return SequenceEqualsExpr(fieldName, fieldType.GenericTypeArguments.Single());
-                }
-
-                if (fieldType.IsValueType || ValueTypeEquivalents.Contains(fieldType))
-                {
-                    return EqualOperatorExpr(fieldName);
-                }
-
-                if (fieldType.GetInterfaces().Any(i =>
-                    i.IsGenericType &&
-                    i.GetGenericTypeDefinition() == typeof(IEquatable<>) &&
-                    i.GenericTypeArguments.Single() == fieldType))
-                {
-                    return
-                        Expression.OrElse(
-                            EqualOperatorExpr(fieldName),
-                            Expression.AndAlso(
-                                Expression.Not(
-                                    Expression.ExclusiveOr(
-                                        Expression.Equal(
-                                            Expression.Field(OneParam, fieldName),
-                                            Expression.Constant(null)),
-                                        Expression.Equal(
-                                            Expression.Field(OtherParam, fieldName),
-                                            Expression.Constant(null)))),
-                                EqualsExpr(fieldName, fieldType)));
-                }
-
-                throw new ArgumentException($"cannot handle field {fieldName} of type {fieldType.FullName}");
-            }
-
-            MethodCallExpression SequenceEqualsExpr(string fieldName, Type elementType)
-            {
-                var enumerableType = typeof(IEnumerable<>).MakeGenericType(elementType);
-
-                return Expression.Call(
-                    null,
-                    Methods.SequenceEqual(elementType),
-                    Expression.Convert(
-                        Expression.Field(OneParam, fieldName), enumerableType),
-                    Expression.Convert(
-                        Expression.Field(OtherParam, fieldName), enumerableType));
-            }
-
-            MethodCallExpression EqualsExpr(string fieldName, Type fieldType) =>
-                Expression.Call(
-                    Expression.Field(OneParam, fieldName),
-                    Methods.EquatableEquals(fieldType),
-                    Expression.Field(OtherParam, fieldName));
-
-            BinaryExpression EqualOperatorExpr(string fieldName) =>
-                Expression.Equal(
-                    Expression.Field(OneParam, fieldName),
-                    Expression.Field(OtherParam, fieldName));
         }
 
-        public static Func<T, T, bool> FromProperties<T>()
+        private static Expression EqualityExpr(Expression oneParam, Expression otherParam, string memberName, Type memberType,
+            GetMemberAccessor getMemberAccessor)
         {
-            ParameterExpression OneParam = Expression.Parameter(typeof(T), "one");
-            ParameterExpression OtherParam = Expression.Parameter(typeof(T), "other");
-
-            PropertyInfo[] propertyInfos = typeof(T).GetProperties();
-            if (!propertyInfos.Any())
+            if (memberType.IsGenericType && ValidSequenceTypes.Contains(memberType.GetGenericTypeDefinition()))
             {
-                return Expression.Lambda<Func<T, T, bool>>(
-                    Expression.Constant(true),
-                    OneParam,
-                    OtherParam)
-                    .Compile();
+                return SequenceEqualsExpr(oneParam, otherParam, memberName, memberType.GenericTypeArguments.Single(), getMemberAccessor);
             }
 
-            var equalityExpressions = propertyInfos.Select(EqualityExpr);
-            var andExpression = equalityExpressions.Aggregate(Expression.AndAlso);
-
-            Expression<Func<T, T, bool>> equalsExpression = Expression.Lambda<Func<T, T, bool>>(
-                andExpression,
-                new[] { OneParam, OtherParam });
-
-            return equalsExpression.Compile();
-
-            Expression EqualityExpr(PropertyInfo propertyInfo)
+            if (memberType.IsValueType || ValueTypeEquivalents.Contains(memberType))
             {
-                var propertyName = propertyInfo.Name;
-                var propertyType = propertyInfo.PropertyType;
-                
-                if (propertyType.IsGenericType && ValidSequenceTypes.Contains(propertyType.GetGenericTypeDefinition()))
-                {
-                    return SequenceEqualsExpr(propertyName, propertyType.GenericTypeArguments.Single());
-                }
-
-                if (propertyType.IsValueType || ValueTypeEquivalents.Contains(propertyType))
-                {
-                    return EqualOperatorExpr(propertyName);
-                }
-
-                if (propertyType.GetInterfaces().Any(i =>
-                    i.IsGenericType &&
-                    i.GetGenericTypeDefinition() == typeof(IEquatable<>) &&
-                    i.GenericTypeArguments.Single() == propertyType))
-                {
-                    return
-                        Expression.OrElse(
-                            EqualOperatorExpr(propertyName),
-                            Expression.AndAlso(
-                                Expression.Not(
-                                    Expression.ExclusiveOr(
-                                        Expression.Equal(
-                                            Expression.Property(OneParam, propertyName),
-                                            Expression.Constant(null)),
-                                        Expression.Equal(
-                                            Expression.Property(OtherParam, propertyName),
-                                            Expression.Constant(null)))),
-                                EqualsExpr(propertyName, propertyType)));
-                }
-
-                throw new ArgumentException($"cannot handle property {propertyName} of type {propertyType.FullName}");
+                return EqualOperatorExpr(oneParam, otherParam, memberName, getMemberAccessor);
             }
 
-            MethodCallExpression SequenceEqualsExpr(string propertyName, Type elementType)
+            if (memberType.GetInterfaces().Any(i =>
+                i.IsGenericType &&
+                i.GetGenericTypeDefinition() == typeof(IEquatable<>) &&
+                i.GenericTypeArguments.Single() == memberType))
             {
-                var enumerableType = typeof(IEnumerable<>).MakeGenericType(elementType);
-
-                return Expression.Call(
-                    null,
-                    Methods.SequenceEqual(elementType),
-                    Expression.Convert(
-                        Expression.Property(OneParam, propertyName), enumerableType),
-                    Expression.Convert(
-                        Expression.Property(OtherParam, propertyName), enumerableType));
+                return
+                    Expression.OrElse(
+                        EqualOperatorExpr(oneParam, otherParam, memberName, getMemberAccessor),
+                        Expression.AndAlso(
+                            Expression.Not(
+                                Expression.ExclusiveOr(
+                                    Expression.Equal(
+                                        getMemberAccessor(oneParam, memberName),
+                                        Expression.Constant(null)),
+                                    Expression.Equal(
+                                        getMemberAccessor(otherParam, memberName),
+                                        Expression.Constant(null)))),
+                            EqualsExpr(oneParam, otherParam, memberName, memberType, getMemberAccessor)));
             }
 
-            MethodCallExpression EqualsExpr(string propertyName, Type propertyType) =>
-                Expression.Call(
-                    Expression.Property(OneParam, propertyName),
-                    Methods.EquatableEquals(propertyType),
-                    Expression.Property(OtherParam, propertyName));
-
-            BinaryExpression EqualOperatorExpr(string propertyName) =>
-                Expression.Equal(
-                    Expression.Property(OneParam, propertyName),
-                    Expression.Property(OtherParam, propertyName));
+            throw new ArgumentException($"cannot handle field {memberName} of type {memberType.FullName}");
         }
+
+        private static MethodCallExpression SequenceEqualsExpr(
+            Expression oneParam, Expression otherParam, string memberName, Type elementType, GetMemberAccessor getMemberAccessor)
+        {
+            var enumerableType = typeof(IEnumerable<>).MakeGenericType(elementType);
+
+            return Expression.Call(
+                null,
+                Methods.SequenceEqual(elementType),
+                Expression.Convert(
+                    getMemberAccessor(oneParam, memberName), enumerableType),
+                Expression.Convert(
+                    getMemberAccessor(otherParam, memberName), enumerableType));
+        }
+
+        private static MethodCallExpression EqualsExpr(
+            Expression oneParam, Expression otherParam, string memberName, Type memberType, GetMemberAccessor getMemberAccessor) =>
+            Expression.Call(
+                getMemberAccessor(oneParam, memberName),
+                Methods.EquatableEquals(memberType),
+                getMemberAccessor(otherParam, memberName));
+
+        private static BinaryExpression EqualOperatorExpr(
+            Expression oneParam, Expression otherParam, string fieldName, GetMemberAccessor getMemberAccessor) =>
+            Expression.Equal(
+                getMemberAccessor(oneParam, fieldName),
+                getMemberAccessor(otherParam, fieldName));
+
+        
     }
 }
