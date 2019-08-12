8fe9cb0a12889de043f9f24b185291641e781e5b
Sven Hübner <sven.huebner.it@gmx.de>
2019-08-12 16:05:09 +0200

    add basic Of<T>

---
+++ KindOf/OfT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KindOf
+{
+    public abstract class Of<T>
+    {
+        private readonly T Value;
+
+        public Of(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            Value = value;
+        }
+
+        public override bool Equals(object obj) =>
+            obj != null &&
+            obj.GetType() == GetType() &&
+            Equals(Value, ((Of<T>)obj).Value);
+
+        public override int GetHashCode() =>
+            GetType().GetHashCode() * 31 + Value.GetHashCode();
+
+        public static implicit operator T(Of<T> of) => of.Value;
+    }
+}
